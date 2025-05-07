9443a8989901f69ed9a7d0bf43934488b5ff5acb
afrid0126 <afridk2601@gmail.com>
2025-05-07 07:26:52 +0530

    change the navbar to add logo

--- src/HOME/Header.jsx
+++ src/HOME/Header.jsx
@@ -6,45 +6,40 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <nav className="custom-navbar navbar navbar-expand-lg mx-auto mt-3">
-      <button
-        className="navbar-toggler mx-auto"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <>
+      <nav className="navbar navbar-expand-lg custom-navbar mt-3">
+        <div className="container-fluid d-flex justify-content-between align-items-center px-3">
+          <Link className="navbar-brand d-flex align-items-center" to="/">
+            <img src="/TEDx/assets/tedex.png" alt="TEDx GPREC" className="logo-img" />
+          </Link>
 
-      <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-        <ul className="navbar-nav text-center">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/theme">Theme</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/speakers">Speakers</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/performers">Performers</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/team">Team</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+
+        {/* Centered Nav Items */}
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul className="navbar-nav text-center">
+            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/theme">Theme</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/speakers">Speakers</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/performers">Performers</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/team">Team</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
